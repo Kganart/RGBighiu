09c20b4d8a7e0041ed80e2d5f8758b02e995bd3d
Kganart <148338115+Kganart@users.noreply.github.com>
2024-08-28 11:30:38 +0100

    Update 3dcard.js

--- assets/js/3dcard.js
+++ assets/js/3dcard.js
@@ -1,6 +1,10 @@
-let card = document.querySelector('.box');
-  document.addEventListener('mousemove', function (e) {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+let cards = document.querySelectorAll('.box');
+
+document.addEventListener('mousemove', function (e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+
+  cards.forEach(function(card) {
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  });
 });
