b1c2e118deaf8383d24c39bdf4d23fd617868533
Kganart <148338115+Kganart@users.noreply.github.com>
2024-10-15 11:11:45 +0100

    Add files via upload

--- assets/js/3dcard.js
+++ assets/js/3dcard.js
@@ -1,6 +1,8 @@
-bigChange=document.getElementById('changeSize')
-trackNum=0
-
+bigChange=document.getElementById('changeSize');
+trackNum= -1;
+setTimeout(function(){
+  trackNum=0;
+}, 2000);
 //bigChange.addEventListener("animationend", () => bigChange.classList.remove('card-expand'));
 
 //switchFunction = () => bigChange.classList.add('card-expand'); //add the class to play animation
@@ -9,20 +11,25 @@ function switchFunction() {
   if (trackNum==0) {
     bigChange.classList.remove("card-an");
     bigChange.classList.add("card-expand");
-    trackNum=1;
+    setTimeout(function(){
+      trackNum=1;
+    }, 1000);
     return
   }
   if (trackNum==1) {
     bigChange.classList.remove("card-expand");
     bigChange.classList.add("card-close");
-    trackNum=2;
+    setTimeout(function(){
+      trackNum=2;
+    }, 1000);
     return
   }
   else if (trackNum==2) {
     bigChange.classList.remove("card-close");
     bigChange.classList.add("card-expand");
-    trackNum=1;
-    
+    setTimeout(function(){
+      trackNum=1;
+    }, 1000);
     return
   }
 
