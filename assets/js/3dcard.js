20cf0b79643023407c84e620e143a7d4a8d66460
Kganart <148338115+Kganart@users.noreply.github.com>
2024-10-17 12:33:04 +0100

    Add files via upload

--- assets/js/3dcard.js
+++ assets/js/3dcard.js
@@ -1,3 +1,143 @@
+// FOOTER
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+<footer>
+    <div class="footer-contain bg-black text-white">
+        <div class="justify-content-center">
+            <div class="mx-auto row justify-content-center">
+                <div class="mx-auto row justify-content-center px-2 text-center">
+                    <div class="col">
+                        <!-- Instagram -->
+                        <a target="_blank" href="https://www.instagram.com/r.g.bighiu"><i
+                                class='bx bxl-instagram about-icons justify-content-center align-self-center'></i></a>
+                        <!-- Youtube -->
+                        <a target="_blank" href="https://www.youtube.com/@r.g.bighiu"><i
+                                class='bx bxl-youtube about-icons justify-content-center align-self-center'></i></a>
+                        <!-- Medium -->
+                        <a target="_blank" href="https://medium.com/@Rgbighiu"><i
+                                class='bx bxl-medium about-icons justify-content-center align-self-center'></i></a>
+                        <!-- Linkedin -->
+                        <a target="_blank" href="https://www.linkedin.com/in/r-g-bighiu-13740795"><i
+                                class='bx bxl-linkedin about-icons justify-content-center align-self-center'></i></a>
+                    </div>
+                </div>
+                <div class="col-3 col-sm-2 px-0 justify-content-center align-self-center book-store">
+                    <a target="_blank" href="https://www.amazon.co.uk/Tarot-Cards-other-poems-Bighiu/dp/B0BS8XB78F">
+                        <div class="book">
+                        </div>
+                    </a>
+                </div>
+                <h5 class="text-center card-title bottom-block">© 2024 R.G. Bighiu. All rights reserved.</h5>
+            </div>
+        </div>
+    </div>
+
+</footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
+
+// HEADER
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+<nav data-bs-theme="dark" class="navbar navbar-expand-lg px-2 mb-2 sticky-top">
+        <div class="container-fluid">
+            <a class="justify-content-start navbar-brand align-start line-1 anim-typewriter" href="#">R.G. Bighiu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse justify-content-end navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav  my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 350px;">
+                    <li class="nav-item">
+                        <a class="justify-content-end text-center nav-link" aria-current="page"
+                            href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-center justify-content-end" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Writing
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item text-center" href="essays.html">Essays</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-center" href="poems.html">Poems</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-center" href="short-stories.html">Short Stories</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-center" href="cards.html">All</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="justify-content-end text-center nav-link" href="about.html">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
+
+// MANDALAS
+
+class Mandalas extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+<div class="mandalaLHS1 position-fixed start-0 translate-middle">
+        <img src="assets/images/mand2.png" alt="Mandala" class="mandala1  object-fit-contain img-fluid mobile-max-width"
+            autoplay></img>
+    </div>
+    <div class="mandalaRHS1 position-fixed start-100 translate-middle">
+        <img src="assets/images/mand1.png" alt="Mandala" class="mandala2  object-fit-contain img-fluid mobile-max-width"
+            autoplay></img>
+    </div>
+    <div class="mandalaRHS2 position-fixed start-100 translate-middle">
+        <img src="assets/images/mand2.png" alt="Mandala" class="mandala3  object-fit-contain img-fluid mobile-max-width"
+            autoplay></img>
+    </div>
+    `;
+  }
+}
+
+customElements.define('mandalas-component', Mandalas);
+
+
+// EXPAND CARD
+
 bigChange=document.getElementById('changeSize');
 trackNum= -1;
 setTimeout(function(){
